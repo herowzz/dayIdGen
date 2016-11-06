a63a807269e1fab63f1384cb92733a11f01f3760
herowzz <herowzz@qq.com>
2016-11-06 23:27:11 +0800

    init

---
+++ src/main/java/com/github/herowzz/dayIdGen/App.java
@@ -0,0 +1,13 @@
+package com.github.herowzz.dayIdGen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
